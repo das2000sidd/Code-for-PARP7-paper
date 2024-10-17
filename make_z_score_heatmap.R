daf805cb9f8c603b20f41f5db26d6a8fa26d0ecf
Siddhartha Das <das2000sidd@gmail.com>
2024-10-17 22:19:35 +0200

    Update make_z_score_heatmap.R

--- make_z_score_heatmap.R
+++ make_z_score_heatmap.R
@@ -5,16 +5,8 @@ library(dplyr)
 
 parp7ko=read.csv(file="PARP7KO_vs_WT.csv",header = T,stringsAsFactors = F)
 
-parp7ko_noGm_riken=parp7ko[- grep("RIKEN",parp7ko$Genename),]
-#parp7ko_noGm_riken=parp7ko_noGm_riken[complete.cases(parp7ko_noGm_riken),]
-parp7ko_noGm_riken=parp7ko_noGm_riken[- grep("predicted",parp7ko_noGm_riken$Genename),]
-parp7ko_noGm_riken=parp7ko_noGm_riken[- grep("Riken",parp7ko_noGm_riken$Genename),]
-parp7ko_noGm_riken=parp7ko_noGm_riken[- grep("pseudogene",parp7ko_noGm_riken$Genename),]
-
-
-
-parp7ko_up=subset(parp7ko_noGm_riken,parp7ko_noGm_riken$padj < 0.01 & parp7ko_noGm_riken$log2FoldChange > 1)
-parp7ko_dn=subset(parp7ko_noGm_riken,parp7ko_noGm_riken$padj < 0.01 & parp7ko_noGm_riken$log2FoldChange < -1)
+parp7ko_up=subset(parp7ko,parp7ko$padj < 0.01 & parp7ko$log2FoldChange > 1)
+parp7ko_dn=subset(parp7ko,parp7ko$padj < 0.01 & parp7ko$log2FoldChange < -1)
 
 parp7ko_up=parp7ko_up[order (-parp7ko_up$log2FoldChange),]
 parp7ko_dn=parp7ko_dn[order (parp7ko_dn$log2FoldChange),]
@@ -22,10 +14,6 @@ parp7ko_dn=parp7ko_dn[order (parp7ko_dn$log2FoldChange),]
 
 parp7ko_up_dn=c(parp7ko_dn$Ensembl,parp7ko_up$Ensembl)
 
-
-
-
-  
 cpm=read.table(file="Normalised_CPM_count.txt",header = T,sep="\t",stringsAsFactors = F)
 
 cpm_parp7ko=cpm[,grep("K",colnames(cpm))]
